83208505d1f8af78957a55d5742bf155ee94ce60
2xente <lasserrebixente@gmail.com>
2022-11-09 09:48:01 +0000

    #1 admin show one user

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -14,4 +14,15 @@ class AdminController extends AbstractController
       
         return $this->twig->render('Setting/Admin/admin.html.twig', ['users' => $users]);
     }
+
+    public function showDeleteUser($id): string
+    {
+        $adminManager = new AdminManager();
+        $admin = $adminManager->selectOneUser($id);
+
+        //var_dump($admin);
+        //die();
+
+        return $this->twig->render('Setting/Admin/admin-delete.html.twig', ['admin' => $admin]);
+    }
 }
