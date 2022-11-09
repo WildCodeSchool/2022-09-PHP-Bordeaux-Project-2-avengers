db92e1bd0b7a9096d8285e60ae29d2e94eca7377
AlexisBoucherie <alexis.boucherie@gmail.com>
2022-11-09 16:40:18 +0100

    add comments for homecontroller loading process

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,23 +13,28 @@ class HomeController extends AbstractController
      */
     public function index(): string
     {
+        // on crée une connexion à l'API de Spotify
         $spotify = new SpotifyAPIController();
         $spotify->connectToAPI();
 
+        // on charge un array à deux dimensions avec des IDs de musiques
         $trackList = $spotify->createTrackLists();
 
+        // on distribue les arrays d'IDs dans des variables qu'on passera individuellement à la vue
         $metalMusic = $trackList[0];
         $rockMusic = $trackList[1];
         $popMusic = $trackList[2];
         $electroMusic = $trackList[3];
         $countryMusic = $trackList[4];
 
+        // on passe les IDs en paramètre dans la méthode de l'APIController (objet $spotify, ici)
         $tracksMetal = $spotify->getMultipleTracks($metalMusic);
         $tracksRock = $spotify->getMultipleTracks($rockMusic);
         $tracksPop = $spotify->getMultipleTracks($popMusic);
         $tracksElectro = $spotify->getMultipleTracks($electroMusic);
         $tracksCountry = $spotify->getMultipleTracks($countryMusic);
 
+        // on retourne la vue twig avec les arrays de musiques qu'on a chargées
         return $this->twig->render('Home/index.html.twig',
             [
                 'metal' => $tracksMetal,
