e535a18319208c97ec2f79756b6bd1264d4f27ba
AnthonyMarche <marche.anthony72@gmail.com>
2022-11-11 15:34:12 +0100

    edit profile adap with navbar

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Controller\Service\FormController;
 use App\Model\UserManager;
 
 class UserController extends AbstractController
@@ -17,5 +18,52 @@ class UserController extends AbstractController
         } else {
             return $this->twig->render('Setting/show_profile.html.twig');
         }
+    }
+
+    /**
+     * Edit information for user connected
+     */
+    public function editProfile(): ?string
+    {
+        if (!$this->user) {
+            echo 'Unauthorized access';
+            header('HTTP/1.1 401 Unauthorized');
+        } else {
+            $userManager = new UserManager();
+
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                $service = new FormController();
+
+                $userUpdate = array_map('trim', $_POST);
+                $userUpdate = array_map('htmlspecialchars', $_POST);
+
+                $errors = $service->emptyForm($userUpdate);
+
+                if (!empty($userUpdate['new-password'])) {
+                    if (empty($userUpdate['password'])) {
+                        $errors[] = 'You must write your actual password to change it.';
+                    }
+                    if ($userUpdate['password'] != $this->user['password']) {
+                        $errors[] = 'Actual password is not correct.';
+                    } else {
+                        $userUpdate['password'] = $_POST['new-password'];
+                    }
+                } else {
+                    $userUpdate['password'] = $this->user['password'];
+                }
+
+                if (!empty($errors)) {
+                    return $this->twig->render('Setting/update_profile.html.twig', ['errors' => $errors]);
+                } else {
+                    $userManager->update($userUpdate);
+                    $success = "Your profile has been update with success !";
+                    $userUpdate['email'] = $this->user['email'];
+                    return $this->twig->render('Setting/show_profile.html.twig', array(
+                        'success' => $success,
+                        'user' => $userUpdate));
+                }
+            }
+        }
+        return $this->twig->render('Setting/update_profile.html.twig');
     }
 }
