d88f99f3450b976070825bff36dc71c2e0eecd51
AlexisBoucherie <alexis.boucherie@gmail.com>
2022-11-16 14:58:56 +0100

    manage to add Spotify search on top of local search

--- src/Controller/SearchPageController.php
+++ src/Controller/SearchPageController.php
@@ -12,13 +12,22 @@ class SearchPageController extends AbstractTwigController
     public function searchSongs()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            // search on SPOTIFY (keyword "track" is important, instead "song" will be invalid)
+            $songTitleFoSpotify = $_POST['search'];
+            $spotifySearch = new SpotifyAPIController();
+            $spotifySearch->connectToAPI();
+            $spotifySongList = $spotifySearch->searchTracks($songTitleFoSpotify, 'track');
+            $spotifySongList = $spotifySongList->tracks->items;
+
+            // do a LOCAL search
             $fetchSongsManager = new FetchSongsManager();
             $songs = $fetchSongsManager->songSearch();
             $fetchSongsManagerImg = new FetchSongsManager();
             $rndSongCoverImg = $fetchSongsManagerImg->showImgDir();
 
             return $this->twig->render('/Home/General/searchPage.html.twig', [
-                'songs' => $songs, 'rndSongCoverImg' => $rndSongCoverImg
+                'songs' => $songs, 'rndSongCoverImg' => $rndSongCoverImg, 'spotifySongList' => $spotifySongList
             ]);
         }
     }
