9a5d8537595f7bfcd0a0dbeb390950faae5b6370
AlexisBoucherie <alexis.boucherie@gmail.com>
2022-11-16 13:36:55 +0100

    manage to migrate track info from homepage to music player

--- src/Controller/PlayPageController.php
+++ src/Controller/PlayPageController.php
@@ -22,4 +22,30 @@ class PlayPageController extends AbstractTwigController
                 'song' => $song, 'artistSongs' => $artistSongs, 'rndSongCoverImg' => $rndSongCoverImg
             ]);
     }
+
+    /**
+     * Once you click on a track to play in the homepage, catch the sample info with "$_GET" to feed the music player
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function playSpotifySong()
+    {
+        // get the music sample from the homepage (then from SPOTIFY)
+        $trackURL = $_GET['spotify-sample'];
+        $trackArtist = $_GET['spotify-artist'];
+        $trackTitle = $_GET['spotify-title'];
+        $trackImg = $_GET['spotify-img'];
+
+        // if no sample is available, redirect toward homepage
+        if ($trackURL === '') {
+            header('Location: /');
+            die();
+        }
+        return $this->twig->render('/Home/General/spotifyPlayPage.html.twig',
+            ['trackURL' => $trackURL, 'trackArtist' => $trackArtist,
+                'trackTitle' => $trackTitle, 'trackImg' => $trackImg]);
+    }
 }
